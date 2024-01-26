ddb30ed6161308db2c2a8c3714bde003fc8530e0
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2024-01-26 13:42:50 -0500

    RestServer created

---
+++ models/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+
+    constructor() {        
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Middlewares
+        this.middlewares();
+
+
+        // Rutas de la aplicación
+        this.routes();
+
+    }
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Directorio público
+        this.app.use( express.static('public') );
+
+    }
+
+    routes() {
+        this.app.use('/api/usuarios', require('../routes/user'));
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log( 'Servidor escuchando en el puerto', this.port );
+        })
+    }
+
+}
+
+module.exports = Server;
