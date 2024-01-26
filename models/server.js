bdc1f7c0e50dd8df26e0c1c34460971228bffa58
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2024-01-26 15:21:46 -0500

    RestServer: routes and controllers dir created

--- models/server.js
+++ models/server.js
@@ -6,6 +6,7 @@ class Server {
     constructor() {        
         this.app = express();
         this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
 
         // Middlewares
         this.middlewares();
@@ -27,7 +28,7 @@ class Server {
     }
 
     routes() {
-        this.app.use('/api/usuarios', require('../routes/user'));
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
     }
 
     listen() {
