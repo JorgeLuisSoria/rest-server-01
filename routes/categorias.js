0e24747092d4a6f6b560c98000b272892190342f
Jorge Luis Soria <100111620+JorgeLuisSoria@users.noreply.github.com>
2024-02-04 22:45:17 -0500

    Categories routes updated

--- routes/categorias.js
+++ routes/categorias.js
@@ -4,8 +4,33 @@ const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
+/**
+ * {{url}}/api/categorias
+ */
+
+// Obtener todas las categorias - público
 router.get('/', (req, res) => {
-    res.json('Todo OK');
+    res.json('get');
+});
+
+// Obtener una categoría por Id - público
+router.get('/:id', (req, res) => {
+    res.json('get - id');
+});
+
+// Crear categoría - privado con cualquier rol con token válido
+router.post('/', (req, res) => {
+    res.json('post');
+});
+
+// Actualizar - privado - cualquiera con token válido
+router.put('/:id', (req, res) => {
+    res.json('put');
+});
+
+// Borrar categoría - Admin role
+router.delete('/:id', (req, res) => {
+    res.json('delete');
 });
 
 module.exports = router;
